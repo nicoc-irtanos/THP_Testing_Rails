b01c3a3547046a67203eacde87ff83ab0ec0726a
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-06 15:25:27 +0200

    test début

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "prénom vide" do
+    util = User.new
+    util.last_name = ""
+    util.first_name = "woof"
+    assert_not util.save, "il ne faut pas l'enregister"
+  end
+  test "prénom d'espaces" do
+    util = User.new
+    util.last_name = "         "
+    util.first_name = "woof"
+    assert_not util.save, "il ne wooof lel ke  opeeiii"
+  end
 end
