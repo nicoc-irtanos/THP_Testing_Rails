5e4cd29d43a860a4f84808b3789ecf7c534e649c
nicolasculerier <nicolas.culerier@laposte.net>
2018-08-06 12:30:58 +0200

    Nico a PostGre

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
     # create a new session
     # first get the user
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # log_in (go to sessions_helper)
       log_in user
-      redirect_to "/secret_page", flash: {success: 'Connecté !'}
+      redirect_to "/club", flash: {success: 'Connecté !'}
     else
       redirect_to "/login", flash: { danger: 'Mauvaise combinaison email/mot de passe. Essayez de nouveau'}
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,47 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+      redirect_to("/users/" + @user.id.to_s)
+      flash.now[:success] = "Profil utilisateur créé"
+      log_in @user
+    else
+      render 'new'
+      # i don't think this flash[:fail] is used
+      flash.now[:fail] = "Passwords doesn't match or too short "
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
+  def edit
+    @user = current_user
+    # if no user logged
+    if @user == nil
+      redirect_to login_path
+    # go to edit page
+    elsif @user.id == params[:id].to_i
+    # else case are wrong users
+    else
+      redirect_to login_path
+    end
+  end
+
+  def delete
+  end
 end
