4035e3748bd9c2a7253a17c7c19ae05db75f1303
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-06 12:49:51 +0200

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_083003) do
+ActiveRecord::Schema.define(version: 2018_08_06_104230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_08_06_083003) do
     t.string "email"
     t.string "first_name"
     t.string "last_name"
+    t.string "password_digest"
   end
 
 end
