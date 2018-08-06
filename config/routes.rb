5f61ea1ea1e58240f0bd59b915779e20cbd92ae2
nicolasculerier <nicolas.culerier@laposte.net>
2018-08-06 11:50:56 +0200

    Home, club etc...

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get '/club', to: 'static_pages#club'
+
+  resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
 end
