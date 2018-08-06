ee5e2c1e4db712900c2dc284325678f083f32a3b
nicolasculerier <nicolas.culerier@laposte.net>
2018-08-06 23:57:06 +0200

    Seed rempli

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: "Nicolas", last_name: "Culerier", email: "nicolas.culerier@laposte.net", password: "bonjour", password_confirmation: "bonjour")
+User.create(first_name: "Jean", last_name: "Dupont", email: "jean@sabug.com", password: "irtanos", password_confirmation: "irtanos")
+User.create(first_name: "Nicolas", last_name: "Zhao", email: "n.zhao@hotmail.fr", password: "loliloli", password_confirmation: "loliloli" )
